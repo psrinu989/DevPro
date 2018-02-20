e6de328c79abb0667bf72f1dd49d8f07dab637cc
Ponneri Srinivasulu Reddy <srinivasulu.ponneri@gmail.com>
2018-02-21 00:14:45 +0530

    committed by sirni

---
+++ src/main/java/org/srinu/MavenDemo/App.java
@@ -0,0 +1,13 @@
+package org.srinu.MavenDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
